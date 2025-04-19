d58c38f8516a2d4a7a3be011842efad0511edb30
IvanDrf <ivandrgmr@gmail.com>
2025-04-19 23:54:05 +0300

    init commit

---
+++ App/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/IvanDrf/Account-system/App/storage"
+)
+
+func main() {
+	fmt.Println("2. Register")
+	fmt.Println("1. Log in")
+	fmt.Print("choice: ")
+
+	choice := 0
+	fmt.Scan(&choice)
+
+	switch choice {
+	case 1:
+
+	}
+
+}
